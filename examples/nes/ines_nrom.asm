21b38371bd2774c894df082c10018f1d296cc806
Henrique Lorenzi <hlorenzi12@gmail.com>
2018-03-26 23:23:28 -0300

    add working nes program

---
+++ examples/nes/ines_nrom.asm
@@ -0,0 +1,33 @@
+#bankdef "header"  { #addr 0x0,    #size 0x10,   #outp 0x0    }
+#bankdef "prg"     { #addr 0x8000, #size 0x7ffa, #outp 0x10   }
+#bankdef "vectors" { #addr 0x7ffa, #size 0x6,    #outp 0x800a }
+
+
+#bank "header"
+
+; magic number
+#str "NES"
+#d8 0x1a
+
+#d8 2 ; 16KB PRG bank count
+#d8 0 ; 8KB CHR bank count
+#d4 0 ; low nybble of mapper id
+#d1 0
+#d1 0 ; trainer presence
+#d1 0 ; SRAM presence
+#d1 0 ; mirroring
+#d4 0 ; high nybble of mapper id
+#d4 0
+#d8 0
+#d8 0
+#d2 0
+#d1 0 ; bus conflict presence
+#d1 0 ; extra RAM presence
+#d2 0
+#d2 0 ; region
+
+
+#bank "vectors"
+#d16   nmi[7:0] @   nmi[15:8]
+#d16 reset[7:0] @ reset[15:8]
+#d16   irq[7:0] @   irq[15:8]
---
+++ examples/nes/main.asm
@@ -0,0 +1,115 @@
+#include "6502.cpu"
+#include "ines_nrom.asm"
+#include "nes.asm"
+
+
+varTimer = 0x00
+varPaletteIndex = 0x01
+
+
+#bank "prg"
+
+reset:
+	; disable IRQs and decimal mode
+	sei
+	cld
+	
+	; disable APU frame IRQ
+	ldx #0x40
+	stx APU_FRMCNTR
+	
+	; set up stack
+	ldx #0xff
+	txs
+	
+	; disable NMI	
+	inx
+	stx PPU_CTRL
+	
+	; disable rendering
+	stx PPU_MASK
+	
+	; disable DMC IRQs
+	stx APU_DMC
+
+	; wait for PPU to be ready
+	.vblankwait1:
+		bit PPU_STATUS
+		bpl .vblankwait1
+	
+	; clear memory
+	.clearmem:
+		lda #0x00
+		sta 0x0000, x
+		sta 0x0100, x
+		sta 0x0200, x
+		sta 0x0300, x
+		sta 0x0400, x
+		sta 0x0500, x
+		sta 0x0600, x
+		sta 0x0700, x
+		inx
+		bne .clearmem
+		
+	; wait for PPU to be ready again
+	.vblankwait2:
+		bit PPU_STATUS
+		bpl .vblankwait2
+		
+	; enable rendering
+	lda #PPU_MASK_SHOWBKG | PPU_MASK_LEFTBKG
+	sta PPU_MASK
+	
+	; enable NMI
+	lda #PPU_CTRL_NMI
+	sta PPU_CTRL
+	
+	; infinite loop
+	.infinite:
+		jmp .infinite
+	
+	
+nmi:
+	; increment timer
+	inc varTimer
+	lda varTimer
+	cmp #8
+	bne .end
+	
+	; if timer reached 8...
+		lda #0
+		sta varTimer
+		
+		; update background color
+		lda PPU_STATUS
+		
+		lda #VRAM_PALETTE[15:8]
+		sta PPU_ADDR
+		lda #VRAM_PALETTE[7:0]
+		sta PPU_ADDR
+		
+		ldx varPaletteIndex
+		lda palette, x
+		sta PPU_DATA
+		
+		; increment palette index
+		inc varPaletteIndex
+		lda varPaletteIndex
+		cmp #paletteLen
+		bne .end
+		
+		; if pallete index reached the end of the table...
+			lda #0
+			sta varPaletteIndex
+
+.end:
+irq:
+	rti
+	
+	
+palette:
+	#d8 0x0d, 0x01, 0x12, 0x21, 0x31, 0x21, 0x12, 0x01, 0x0d ; blues
+	#d8 0x0d, 0x06, 0x16, 0x26, 0x36, 0x26, 0x16, 0x06, 0x0d ; reds
+	#d8 0x0d, 0x09, 0x19, 0x29, 0x39, 0x29, 0x19, 0x09, 0x0d ; greens
+
+paletteLen = pc - palette
