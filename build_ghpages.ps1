e9efbe4c5f7926ad188a5ab1ad9da8bbecb56157
Henrique Lorenzi <hlorenzi@users.noreply.github.com>
2021-03-20 16:36:14 -0300

    add ghpages build script

--- build_ghpages.ps1
+++ build_ghpages.ps1
@@ -1,3 +1,11 @@
+# Checkout a clean ghpages branch
+git branch -D ghpages
+git checkout -b ghpages
+
+# Replace .gitignore
+Remove-Item -Path .gitignore
+Copy-Item -Path .gitignore.ghpages -Destination .gitignore
+
 # Build wasm binary
 cargo build --lib --target wasm32-unknown-unknown --release
 
@@ -7,3 +15,10 @@ wasm-gc "./target/wasm32-unknown-unknown/release/customasm.wasm" -o "./target/wa
 # Copy to web folder
 Copy-Item -Path "./target/wasm32-unknown-unknown/release/customasm.gc.wasm" -Destination "./web/customasm.gc.wasm"
 
+# Commit to git
+git add -A
+git commit -m "build GitHub Pages"
+git push -f origin ghpages
+
+# Return to main branch
+git checkout main
