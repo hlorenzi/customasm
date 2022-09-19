87f2e10e373cce00ff845c268de9d044127cef29
Joseph Abbey <joseph@abbey1.org.uk>
2022-09-19 13:16:06 +0100

    errors link to the source in web editor

--- web/main.js
+++ web/main.js
@@ -109,6 +109,12 @@ function assemble()
 	dropRustString(outputPtr)
 	
 	output = output.replace(/\n/g, "<br>")
+	output = output.replace(
+		/ --> asm:\x1b\[0m\x1b\[90m(\d+):(\d+)/g,
+		(_, line, column) =>
+			` --> asm:<button class="a" onclick="g_codeEditor.focus();g_codeEditor.setCursor({line:${line - 1
+			},ch:${column - 1}})">${line}:${column}</button>`
+	);
 	output = output.replace(/\x1b\[90m/g, "</span><span style='color:gray;'>")
 	output = output.replace(/\x1b\[91m/g, "</span><span style='color:red;'>")
 	output = output.replace(/\x1b\[93m/g, "</span><span style='color:#f80;'>")
@@ -116,6 +122,7 @@ function assemble()
 	output = output.replace(/\x1b\[97m/g, "</span><span style='color:black;'>")
 	output = output.replace(/\x1b\[1m/g, "</span><span style='font-weight:bold;'>")
 	output = output.replace(/\x1b\[0m/g, "</span><span style='color:black;'>")
+	//</span>
 	
 	output = "<span style='color:black;'>" + output + "</span>"
 	
